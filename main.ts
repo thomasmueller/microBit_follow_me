9d681f9c37d62cabea4cdaac12d95c39917e150d
Thomas Mueller <thomas.tom.mueller@gmail.com>
2024-08-28 11:20:10 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,334 @@
+function arrow_left3 () {
+    if (arrow_left2 == 0) {
+        led.plot(0, 2)
+        led.plot(1, 1)
+        led.plot(1, 2)
+        led.plot(1, 3)
+        led.plot(2, 0)
+        led.plot(2, 2)
+        led.plot(2, 4)
+        led.plot(3, 2)
+        led.plot(4, 2)
+    }
+    if (arrow_left2 == 1) {
+        led.plot(0, 1)
+        led.plot(0, 2)
+        led.plot(0, 3)
+        led.plot(1, 0)
+        led.plot(1, 2)
+        led.plot(1, 4)
+        led.plot(2, 2)
+        led.plot(3, 2)
+    }
+    if (arrow_left2 == 2) {
+        led.plot(0, 0)
+        led.plot(0, 2)
+        led.plot(0, 4)
+        led.plot(1, 2)
+        led.plot(2, 2)
+    }
+    if (arrow_left2 == 3) {
+        led.plot(0, 2)
+        led.plot(1, 2)
+        led.plot(4, 2)
+    }
+    if (arrow_left2 == 4) {
+        led.plot(0, 2)
+        led.plot(3, 2)
+        led.plot(4, 1)
+        led.plot(4, 2)
+        led.plot(4, 3)
+    }
+    if (arrow_left2 == 5) {
+        led.plot(2, 2)
+        led.plot(3, 1)
+        led.plot(3, 2)
+        led.plot(3, 3)
+        led.plot(4, 0)
+        led.plot(4, 2)
+        led.plot(4, 4)
+    }
+    if (arrow_left2 == 6) {
+        led.plot(1, 2)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(3, 0)
+        led.plot(3, 2)
+        led.plot(3, 4)
+        led.plot(4, 2)
+    }
+    basic.pause(100)
+}
+function arrow_Down2 () {
+    if (arrow_down2 == 0) {
+        led.plot(0, 2)
+        led.plot(1, 3)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.plot(3, 3)
+        led.plot(4, 2)
+    }
+    if (arrow_down2 == 1) {
+        led.plot(0, 3)
+        led.plot(1, 4)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.plot(3, 4)
+        led.plot(4, 3)
+    }
+    if (arrow_down2 == 2) {
+        led.plot(0, 4)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.plot(4, 4)
+    }
+    if (arrow_down2 == 3) {
+        led.plot(2, 0)
+        led.plot(2, 3)
+        led.plot(2, 4)
+    }
+    if (arrow_down2 == 4) {
+        led.plot(1, 0)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        led.plot(2, 4)
+        led.plot(3, 0)
+    }
+    if (arrow_down2 == 5) {
+        led.plot(0, 0)
+        led.plot(1, 1)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(3, 1)
+        led.plot(4, 0)
+    }
+    if (arrow_down2 == 6) {
+        led.plot(0, 1)
+        led.plot(1, 2)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(3, 2)
+        led.plot(4, 1)
+    }
+    basic.pause(100)
+}
+function arrow_right2 () {
+    if (arrow_right1 == 0) {
+        led.plot(0, 2)
+        led.plot(1, 2)
+        led.plot(2, 0)
+        led.plot(2, 2)
+        led.plot(2, 4)
+        led.plot(3, 1)
+        led.plot(3, 2)
+        led.plot(3, 3)
+        led.plot(4, 2)
+    }
+    if (arrow_right1 == 1) {
+        led.plot(1, 2)
+        led.plot(2, 2)
+        led.plot(3, 0)
+        led.plot(3, 2)
+        led.plot(3, 4)
+        led.plot(4, 1)
+        led.plot(4, 2)
+        led.plot(4, 3)
+    }
+    if (arrow_right1 == 2) {
+        led.plot(2, 2)
+        led.plot(3, 2)
+        led.plot(4, 0)
+        led.plot(4, 2)
+        led.plot(4, 4)
+    }
+    if (arrow_right1 == 3) {
+        led.plot(0, 2)
+        led.plot(3, 2)
+        led.plot(4, 2)
+    }
+    if (arrow_right1 == 4) {
+        led.plot(0, 1)
+        led.plot(0, 2)
+        led.plot(0, 3)
+        led.plot(1, 2)
+        led.plot(4, 2)
+    }
+    if (arrow_right1 == 5) {
+        led.plot(0, 0)
+        led.plot(0, 2)
+        led.plot(0, 4)
+        led.plot(1, 1)
+        led.plot(1, 2)
+        led.plot(1, 3)
+        led.plot(2, 2)
+    }
+    if (arrow_right1 == 6) {
+        led.plot(0, 2)
+        led.plot(1, 0)
+        led.plot(1, 2)
+        led.plot(1, 4)
+        led.plot(3, 1)
+        led.plot(3, 2)
+        led.plot(3, 3)
+        led.plot(4, 2)
+    }
+    basic.pause(100)
+}
+function arrow_up2 () {
+    if (arror_up1 == 0) {
+        led.plot(0, 2)
+        led.plot(1, 1)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.plot(3, 1)
+        led.plot(4, 2)
+    }
+    if (arror_up1 == 1) {
+        led.plot(0, 1)
+        led.plot(1, 0)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(3, 0)
+        led.plot(4, 1)
+    }
+    if (arror_up1 == 2) {
+        led.plot(0, 0)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(4, 0)
+    }
+    if (arror_up1 == 3) {
+        led.plot(2, 0)
+        led.plot(2, 1)
+        led.plot(2, 4)
+    }
+    if (arror_up1 == 4) {
+        led.plot(1, 4)
+        led.plot(2, 0)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.plot(3, 4)
+    }
+    if (arror_up1 == 5) {
+        led.plot(0, 4)
+        led.plot(1, 3)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.plot(3, 3)
+        led.plot(4, 4)
+    }
+    if (arror_up1 == 6) {
+        led.plot(0, 3)
+        led.plot(1, 2)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.plot(3, 2)
+        led.plot(4, 3)
+    }
+    basic.pause(100)
+}
+let GOING = 0
+let arror_up1 = 0
+let arrow_right1 = 0
+let arrow_down2 = 0
+let arrow_left2 = 0
+let FB = 0
+for (let index = 0; index < 3; index++) {
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+}
+basic.forever(function () {
+    if (Proxi.LBlock(310) && Proxi.RBlock(310)) {
+        arror_up1 = arror_up1 + 1
+        Proxi.rückwärts()
+        arrow_up2()
+        music.playTone(392, music.beat(BeatFraction.Eighth))
+        FB = 1
+    } else if (!(Proxi.LBlock(400)) && Proxi.RBlock(400)) {
+        arrow_right1 = arrow_right1 + 1
+        Proxi.linksdrehung()
+        arrow_right2()
+        GOING = -1
+    } else if (Proxi.LBlock(400) && !(Proxi.RBlock(400))) {
+        arrow_left2 = arrow_left2 + 1
+        Proxi.rechtsdrehung()
+        arrow_left3()
+        GOING = 1
+    } else if (Proxi.LBlock(200) && Proxi.RBlock(200)) {
+        music.playTone(330, music.beat(BeatFraction.Eighth))
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        if (GOING > 0) {
+            Proxi.linksdrehung()
+            control.waitMicros(500)
+            GOING = 0
+            Proxi.stehenbleiben()
+        }
+        if (GOING < 0) {
+            Proxi.rechtsdrehung()
+            control.waitMicros(500)
+            GOING = 0
+            Proxi.stehenbleiben()
+        }
+        if (FB > 0) {
+            Proxi.vorwärts()
+            control.waitMicros(500)
+            Proxi.stehenbleiben()
+        }
+        if (FB < 0) {
+            Proxi.rückwärts()
+            control.waitMicros(500)
+            Proxi.stehenbleiben()
+        }
+        GOING = 0
+        FB = 0
+    } else if (Proxi.LBlock(50) && Proxi.RBlock(50)) {
+        arrow_down2 = arrow_down2 + 1
+        Proxi.vorwärts()
+        arrow_Down2()
+        music.playTone(330, music.beat(BeatFraction.Eighth))
+        FB = -1
+    } else {
+        GOING = 0
+        FB = 0
+        Proxi.stehenbleiben()
+        Proxi.drehungsstopp()
+    }
+    basic.clearScreen()
+    if (arror_up1 == 6) {
+        arror_up1 = 0
+    }
+    if (arrow_down2 == 6) {
+        arrow_down2 = 0
+    }
+    if (arrow_left2 == 6) {
+        arrow_left2 = 0
+    }
+    if (arrow_right1 == 6) {
+        arrow_right1 = 0
+    }
+})
